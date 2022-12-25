1590fd715d0c8168e74b8bf5eb5f661702e115a4
ZWD <490857147@qq.com>
2022-12-25 11:07:40 +0800

    正常

--- src/main/java/com/test/FirstTest.java
+++ src/main/java/com/test/FirstTest.java
@@ -4,14 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-0
-1
 @SpringBootApplication
 public class FirstTest {
     public static void main(String[] args) {
-        4
-        SpringApplication.run(FirstTest.class,args);2
-                3
-                        4
+        SpringApplication.run(FirstTest.class,args);
     }
 }
