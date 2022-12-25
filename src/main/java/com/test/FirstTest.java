765099d6cda300ec2c4b30fc16c310baf60e8de6
ZWD <490857147@qq.com>
2022-12-25 10:40:39 +0800

    1

--- src/main/java/com/test/FirstTest.java
+++ src/main/java/com/test/FirstTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstTest {
     public static void main(String[] args) {
-        SpringApplication.run(FirstTest.class,args);
+        SpringApplication.run(FirstTest.class,args);1
     }
 }
 3
