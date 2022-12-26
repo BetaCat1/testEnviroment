b3d3aada886ce3daccf09223a9d38252e00aed91
ZWD <490857147@qq.com>
2022-12-26 13:46:28 +0800

    测试

---
+++ src/test/java/com/test/FirstTestTest.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class FirstTestTest extends TestCase {
+@Test
+    public void testMain() {
+        System.out.println("test");
+
+    }
+}
