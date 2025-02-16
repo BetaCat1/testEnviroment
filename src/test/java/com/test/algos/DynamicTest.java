9da57e4a0f4605d2bd3a35589c46d20c0b05762d
ZWD <490857147@qq.com>
2025-02-16 20:03:16 +0800

    珠宝的最高价值

--- src/test/java/com/test/algos/DynamicTest.java
+++ src/test/java/com/test/algos/DynamicTest.java
@@ -230,4 +230,49 @@ public class DynamicTest {
         return res;
 
     }
+
+
+
+    @Test
+    @DisplayName("珠宝的最高价值")
+    public void test08() {
+
+        int[][] frame  = new int[][]{{1,2,5},{3,2,1}};
+
+        System.out.println(jewelleryValue(frame));
+
+    }
+
+    public int jewelleryValue(int[][] frame) {
+
+        int col = frame.length;
+        int row = frame[0].length;
+
+        int[][] dp = new int[col][row];
+
+        dp[0][0] = frame[0][0];
+
+        if(col==1&&row==1){
+            return dp[0][0];
+        }
+        for (int i = 1; i <= col - 1; i++) {
+            dp[i][0] = dp[i-1][0]+frame[i][0];
+        }
+        for (int j = 1; j <= row - 1; j++) {
+            dp[0][j] = dp[0][j-1]+frame[0][j];
+        }
+
+        for (int i = 1; i <= col - 1; i++) {
+            for (int j = 1; j <= row - 1; j++) {
+
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + frame[i][j];
+            }
+
+
+        }
+
+        return dp[col - 1][row-1];
+    }
+
+
 }
